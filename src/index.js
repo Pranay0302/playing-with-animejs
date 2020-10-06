320c3ed2dcc04252771e6a6982f8e70d8feb5830
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2020-10-06 19:14:07 +0530

    added y component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ console.log("checking..")
 
 anime({
     targets: 'div.box.small',
-    translateX: [
-        { value: 500, duration: 2000 }
-    ],
+    translateX: [100, 600],
     rotate: {
         value: 360,
         duration: 1000,
@@ -16,5 +14,22 @@ anime({
     easing: 'easeInOutQuad',
     direction: 'alternate',
     endDelay: 500,
+    duration: 2000,
+    loop: true
+})
+
+anime({
+    targets: 'div.small2',
+    translateY: [100, 500],
+    rotate: {
+        value: '*=5.4',
+        duration: 500,
+        easing: 'easeInOutSine'
+    },
+    borderRadius: ['0%', '50%'],
+    direction: 'alternate',
+    endDelay: 500,
+    easing: 'easeInOutQuad',
+    duration: 2000,
     loop: true
 })
